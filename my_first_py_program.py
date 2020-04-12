9d3fa4ff026d3b6a91aacf8bab6a847745b56fb5
Shubham Chakraverty <58749337+shubhamchakraverty@users.noreply.github.com>
2020-04-13 01:13:12 +0530

    Adding code to find sum of two numbers

--- my_first_py_program.py
+++ my_first_py_program.py
@@ -1,1 +1,6 @@
 #My first python program on github
+
+num1 = float (input ("Enter the first number : "))
+num2 = float (input ("Enter the second number : "))
+sum = num1 + num2
+print ("The sum of the two numbers is = ",sum)
